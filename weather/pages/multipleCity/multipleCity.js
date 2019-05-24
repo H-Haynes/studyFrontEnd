a60cf32081d94bf81965f55aff693ab5241e6816
Hehong <286683812@qq.com>
2019-05-24 20:28:18 +0800

    天气小程序

--- weather/pages/multipleCity/multipleCity.js
+++ weather/pages/multipleCity/multipleCity.js
@@ -1,15 +1,14 @@
 // pages/multipleCity/multipleCity.js
 const app = getApp();
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    firstLoad:true,
     statusBarHeight: app.globalData.statusBarHeight,
-    cityList:["北京","上海","深圳"],
-    // cityList:[],
+    cityList:[],
     hotCityList:["北京","上海","深圳","广州","杭州","重庆","成都","福州","三亚","香港","澳门"],
     showAdd:false,
     showManage:false,
@@ -20,9 +19,14 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const self = this;
+    this.getCache();
     let str=options.city.slice(0,options.city.length-1);
     //将当前定位位置加入列表
     this.addCity(str);
+    this.setData({
+      firstLoad:false
+    })
   },
 
   /**
@@ -67,20 +71,30 @@ Page({
   backPage(){//页面回退
     wx.navigateBack()
   },
+  getCache(){
+    //获取缓存设置城市列表
+    let storageCityList = wx.getStorageSync("cityList");
+    this.setData({
+      cityList: storageCityList ? storageCityList : ["北京", "上海", "深圳"]
+    })
+  },
   addCity(city){//添加一个城市到列表
     let cityList = this.data.cityList;
     for(let i=0;i<cityList.length;i++){
       if(cityList[i]===city){
-        //已经有了这个城市
-        wx:wx.showToast({
-          title: '已经添加过啦~',
-          icon: 'none',
-          duration: 1500,
-        });
+        //已经有了这个城市了
+        if (!this.data.firstLoad){//不是第一次加载才显示
+          wx: wx.showToast({
+            title: '已经添加过啦~',
+            icon: 'none',
+            duration: 1500,
+          });
+        }
         return
       }
     }
     cityList.unshift(city);//向首部添加城市
+    wx.setStorageSync("cityList", cityList);
     this.setData({  //重设数据
       cityList
     })
@@ -118,14 +132,13 @@ Page({
   deleteCity(e){
       let deleteTarget=e.currentTarget.dataset.city;
     let cityList = this.data.cityList;
-
-
      for(let i=0;i<cityList.length;i++){
        if(cityList[i]===deleteTarget){
          cityList.splice(i,1);
          break;
        }
      }
+    wx.setStorageSync("cityList", cityList);
     
     this.setData({
       cityList
@@ -136,8 +149,5 @@ Page({
     let city=e.currentTarget.dataset.name;
     this.addCity(city);
     this.hideAddBox();
-  },
-  toDetail(e){
-    console.log(e)
   }
 })
